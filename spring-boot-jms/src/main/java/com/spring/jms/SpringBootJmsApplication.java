8085f741ea2a456875bfdf9345c49cb16f5d524e
Razvan Prichici <razvanpaulp@gmail.com>
2022-05-01 20:11:04 +0300

    Add project structure.

---
+++ spring-boot-jms/src/main/java/com/spring/jms/SpringBootJmsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJmsApplication.class, args);
+	}
+
+}
